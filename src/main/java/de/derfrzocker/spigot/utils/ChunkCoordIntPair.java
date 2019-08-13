fc3de05eda83630cb16a7a84898fc359f4c9f87f
DerFrZocker <derrieple@gmail.com>
2019-08-13 18:17:14 +0200

    Add ChunkCoordIntPair
    Took 7 minutes

---
+++ src/main/java/de/derfrzocker/spigot/utils/ChunkCoordIntPair.java
@@ -0,0 +1,45 @@
+package de.derfrzocker.spigot.utils;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Location;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.SerializableAs;
+import org.bukkit.util.NumberConversions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Data
+@RequiredArgsConstructor
+@SerializableAs("DerFrZocker#SpigotUtils#ChunkCoordIntPair")
+public class ChunkCoordIntPair implements ConfigurationSerializable {
+
+    private final int x;
+
+    private final int z;
+
+    public ChunkCoordIntPair(final @NonNull Location location) {
+        this.x = location.getBlockX() >> 4;
+        this.z = location.getBlockZ() >> 4;
+    }
+
+    @Override
+    public Map<String, Object> serialize() {
+        final Map<String, Object> map = new LinkedHashMap<>();
+
+        map.put("x", x);
+        map.put("z", z);
+
+        return map;
+    }
+
+    public static ChunkCoordIntPair deserialize(Map<String, Object> map) {
+        final int x = NumberConversions.toInt(map.get("x"));
+        final int z = NumberConversions.toInt(map.get("z"));
+
+        return new ChunkCoordIntPair(x, z);
+    }
+
+}
