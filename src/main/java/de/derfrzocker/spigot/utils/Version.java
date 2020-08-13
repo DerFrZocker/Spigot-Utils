90e2aa51fa4357ef2221666b98f846d22e45f969
DerFrZocker <derrieple@gmail.com>
2020-08-13 10:49:11 +0200

    add version v1_16_R2
    Took 20 minutes

--- src/main/java/de/derfrzocker/spigot/utils/Version.java
+++ src/main/java/de/derfrzocker/spigot/utils/Version.java
@@ -13,7 +13,7 @@ import java.util.stream.Stream;
 @RequiredArgsConstructor
 public enum Version {
 
-    v1_16_R1(12), v1_15_R1(11), v1_14_R1(10), v1_13_R2(9), v1_13_R1(8), v1_12_R1(7), v1_11_R1(6), v1_10_R1(5), v1_9_R2(4), v1_9_R1(3), v1_8_R3(2), v1_8_R2(1), v1_8_R1(0);
+    v1_16_R2(13), v1_16_R1(12), v1_15_R1(11), v1_14_R1(10), v1_13_R2(9), v1_13_R1(8), v1_12_R1(7), v1_11_R1(6), v1_10_R1(5), v1_9_R2(4), v1_9_R1(3), v1_8_R3(2), v1_8_R2(1), v1_8_R1(0);
 
     @Getter
     private static Version current;
