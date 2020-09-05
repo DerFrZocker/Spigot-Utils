893474294428ebca17f40fe0cf439e40b9ed25f1
DerFrZocker <derrieple@gmail.com>
2020-09-05 15:50:57 +0200

    Rework Version class
    Took 1 hour 30 minutes

--- src/main/java/de/derfrzocker/spigot/utils/Version.java
+++ src/main/java/de/derfrzocker/spigot/utils/Version.java
@@ -1,85 +1,127 @@
 package de.derfrzocker.spigot.utils;
 
-import com.google.common.annotations.VisibleForTesting;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Server;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
+public enum Version implements Comparable<Version> {
 
-@RequiredArgsConstructor
-public enum Version {
-
-    v1_16_R2(13), v1_16_R1(12), v1_15_R1(11), v1_14_R1(10), v1_13_R2(9), v1_13_R1(8), v1_12_R1(7), v1_11_R1(6), v1_10_R1(5), v1_9_R2(4), v1_9_R1(3), v1_8_R3(2), v1_8_R2(1), v1_8_R1(0);
-
-    @Getter
-    private static Version current;
-
-    private final static boolean testcase;
-
-    @Getter
-    private final static boolean paper;
-
-
-    static {
-        paper = Bukkit.getName().equalsIgnoreCase("Paper");
-
-        final String version = Bukkit.getServer().getClass().getPackage().getName().substring(Bukkit.getServer().getClass().getPackage().getName().lastIndexOf('.') + 1);
-
-        if (version.equalsIgnoreCase("codegen") || version.equalsIgnoreCase("bukkit")) { // For junit test, return sometimes "codegen" or "bukkit"
-            testcase = true;
-        } else {
-            testcase = false;
-
-            try {
-                current = valueOf(version.trim());
-            } catch (final IllegalArgumentException e) {
-                throw new IllegalStateException("unknown server version: " + version + " Server class name: " + Bukkit.getServer().getClass().toString());
-            }
-        }
-    }
-
-    public static void clear() {
-        Stream.of(values()).forEach(value -> value.list.clear());
-    }
-
-    @VisibleForTesting
-    public static void setCurrentVersion(final Version version) {
-        if (!testcase)
-            throw new IllegalStateException("Only for Testing!");
-
-        current = version;
-    }
-
-    private final List<Runnable> list = new ArrayList<>();
+    v1_16_R2(13),
+    v1_16_R1(12),
+    v1_15_R1(11),
+    v1_14_R1(10),
+    v1_13_R2(9),
+    v1_13_R1(8),
+    v1_12_R1(7),
+    v1_11_R1(6),
+    v1_10_R1(5),
+    v1_9_R2(4),
+    v1_9_R1(3),
+    v1_8_R3(2),
+    v1_8_R2(1),
+    v1_8_R1(0),
+    UNKNOWN(-1);
 
     private final int value;
 
-    public void run() {
-        list.forEach(Runnable::run);
+    Version(int value) {
+        this.value = value;
     }
 
-    public void add(final @NonNull Runnable runnable) {
-        list.add(runnable);
+    /**
+     * @param server to get the version from
+     * @return the version of the server
+     * @throws IllegalArgumentException if server is null
+     */
+    @NotNull
+    public static Version getServerVersion(@NotNull Server server) {
+        Validate.notNull(server, "Server cannot be null");
+
+        String packageName = server.getClass().getPackage().getName();
+        String version = packageName.substring(packageName.lastIndexOf('.') + 1);
+
+        try {
+            return valueOf(version.trim());
+        } catch (final IllegalArgumentException e) {
+            return Version.UNKNOWN;
+        }
     }
 
-    public boolean isNewerOrSameVersion(final @NonNull Version version) {
-        return version.value >= value;
+    /**
+     * @param server to check
+     * @return true if the server is Paper or false of not
+     * @throws IllegalArgumentException if server is null
+     */
+    public static boolean isPaper(@NotNull Server server) {
+        Validate.notNull(server, "Server cannot be null");
+
+        return server.getName().equalsIgnoreCase("Paper");
     }
 
-    public boolean isNewerVersion(final @NonNull Version version) {
-        return version.value > value;
+    /**
+     * Checks if the version is newer than the given version
+     * <p>
+     * If both versions are the same, the method will return false
+     *
+     * @param version to check against
+     * @return true if the version is newer than the given one, otherwise false
+     * @throws IllegalArgumentException if version is null
+     * @throws IllegalArgumentException if this version or the given version, is the version UNKNOWN
+     */
+    public boolean isNewerThan(@NotNull Version version) {
+        Validate.notNull(version, "Version cannot be null");
+        Validate.isTrue(this != UNKNOWN, "Cannot check, if version UNKNOWN is newer");
+        Validate.isTrue(version != UNKNOWN, "Cannot check, if version UNKNOWN is newer");
+
+        return value > version.value;
     }
 
-    public boolean isOlderOrSameVersion(final @NonNull Version version) {
-        return version.value <= value;
+    /**
+     * Checks if the version is newer or the same than the given version
+     *
+     * @param version to check against
+     * @return true if the version is newer or the same than the given one, otherwise false
+     * @throws IllegalArgumentException if version is null
+     * @throws IllegalArgumentException if this version or the given version, is the version UNKNOWN
+     */
+    public boolean isNewerOrSameThan(@NotNull Version version) {
+        Validate.notNull(version, "Version cannot be null");
+        Validate.isTrue(this != UNKNOWN, "Cannot check, if version UNKNOWN is newer or same");
+        Validate.isTrue(version != UNKNOWN, "Cannot check, if version UNKNOWN is newer or same");
+
+        return value >= version.value;
     }
 
-    public boolean isOlderVersion(final @NonNull Version version) {
-        return version.value < value;
+    /**
+     * Checks if the version is older than the given version
+     *
+     * @param version to check against
+     * @return true if the version is older than the given one, otherwise false
+     * @throws IllegalArgumentException if version is null
+     * @throws IllegalArgumentException if this version or the given version, is the version UNKNOWN
+     */
+    public boolean isOlderThan(@NotNull Version version) {
+        Validate.notNull(version, "Version cannot be null");
+        Validate.isTrue(this != UNKNOWN, "Cannot check, if version UNKNOWN is older");
+        Validate.isTrue(version != UNKNOWN, "Cannot check, if version UNKNOWN is older");
+
+        return value < version.value;
+    }
+
+    /**
+     * Checks if the version is older or the same than the given version
+     *
+     * @param version to check against
+     * @return true if the version is older or the same than the given one, otherwise false
+     * @throws IllegalArgumentException if version is null
+     * @throws IllegalArgumentException if this version or the given version, is the version UNKNOWN
+     */
+    public boolean isOlderOrSameThan(@NotNull Version version) {
+        Validate.notNull(version, "Version cannot be null");
+        Validate.isTrue(this != UNKNOWN, "Cannot check, if version UNKNOWN is older or same");
+        Validate.isTrue(version != UNKNOWN, "Cannot check, if version UNKNOWN is older or same");
+
+        return value <= version.value;
     }
 
 }
