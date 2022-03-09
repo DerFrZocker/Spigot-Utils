ec521c8c23de5ed5adbfe62becd74a0093e7bf4d
DerFrZocker <derrieple@gmail.com>
2022-03-09 12:53:52 +0100

    Add methode to easily get the player from a ClickAction
    Took 3 minutes

--- src/main/java/de/derfrzocker/spigot/utils/guin/ClickAction.java
+++ src/main/java/de/derfrzocker/spigot/utils/guin/ClickAction.java
@@ -1,5 +1,6 @@
 package de.derfrzocker.spigot.utils.guin;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class ClickAction {
@@ -10,6 +11,10 @@ public class ClickAction {
     public ClickAction(InventoryGui inventoryGui, InventoryClickEvent clickEvent) {
         this.inventoryGui = inventoryGui;
         this.clickEvent = clickEvent;
+    }
+
+    public Player getPlayer() {
+        return (Player) getClickEvent().getWhoClicked();
     }
 
     public InventoryClickEvent getClickEvent() {
