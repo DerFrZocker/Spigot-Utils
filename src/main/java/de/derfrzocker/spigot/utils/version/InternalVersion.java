8e2ee78774a0ae27c252e2aa031b3ae458451492
DerFrZocker <git@derfrzocker.de>
2024-06-01 19:21:49 +0200

    Rework how versions are handelt
    Took 42 minutes

---
+++ src/main/java/de/derfrzocker/spigot/utils/version/InternalVersion.java
@@ -0,0 +1,41 @@
+package de.derfrzocker.spigot.utils.version;
+
+public enum InternalVersion {
+
+    v1_20_R4(ServerVersionRange.create("1.20.5", "1.20.6")),
+    v1_20_R3(ServerVersionRange.create("1.20.3", "1.20.4")),
+    v1_20_R2(ServerVersionRange.create("1.20.2", "1.20.2")),
+    v1_20_R1(ServerVersionRange.create("1.20", "1.20.1")),
+    v1_19_R3(ServerVersionRange.create("1.19.4", "1.19.4")),
+    v1_19_R2(ServerVersionRange.create("1.19.3", "1.19.3")),
+    v1_19_R1(ServerVersionRange.create("1.19", "1.19.2")),
+    v1_18_R2(ServerVersionRange.create("1.18.2", "1.18.2")),
+    v1_18_R1(ServerVersionRange.create("1.18", "1.18.1")),
+    v1_17_R1(ServerVersionRange.V1_17),
+    v1_16_R3(ServerVersionRange.create("1.16.4", "1.16.5")),
+    v1_16_R2(ServerVersionRange.create("1.16.2", "1.16.3")),
+    v1_16_R1(ServerVersionRange.create("1.16", "1.16.1")),
+    v1_15_R1(ServerVersionRange.V1_15),
+    v1_14_R1(ServerVersionRange.V1_14),
+    v1_13_R2(ServerVersionRange.create("1.13.1", "1.13.2")),
+    v1_13_R1(ServerVersionRange.create("1.13", "1.13")),
+    v1_12_R1(ServerVersionRange.V1_12),
+    v1_11_R1(ServerVersionRange.V1_11),
+    v1_10_R1(ServerVersionRange.V1_10),
+    v1_9_R2(ServerVersionRange.create("1.9.4", "1.9.4")),
+    v1_9_R1(ServerVersionRange.create("1.9", "1.9.3")),
+    v1_8_R3(ServerVersionRange.create("1.8.4", "1.8.9")),
+    v1_8_R2(ServerVersionRange.create("1.8.3", "1.8.3")),
+    v1_8_R1(ServerVersionRange.create("1.8", "1.8.2")),
+    UNKNOWN(new ServerVersionRange(ServerVersion.NONE, ServerVersion.NONE));
+
+    private final ServerVersionRange serverVersionRange;
+
+    InternalVersion(ServerVersionRange serverVersionRange) {
+        this.serverVersionRange = serverVersionRange;
+    }
+
+    public ServerVersionRange getServerVersionRange() {
+        return this.serverVersionRange;
+    }
+}
---
+++ src/main/java/de/derfrzocker/spigot/utils/version/ServerVersionRange.java
@@ -0,0 +1,32 @@
+package de.derfrzocker.spigot.utils.version;
+
+public record ServerVersionRange(ServerVersion minInclusive, ServerVersion maxInclusive) {
+
+    public static final ServerVersionRange V1_21 = create("1.21", "1.21.1");
+    public static final ServerVersionRange V1_20 = create("1.20", "1.20.6");
+    public static final ServerVersionRange V1_19 = create("1.19", "1.19.4");
+    public static final ServerVersionRange V1_18 = create("1.18", "1.18.2");
+    public static final ServerVersionRange V1_17 = create("1.17", "1.17.1");
+    public static final ServerVersionRange V1_16 = create("1.16", "1.16.5");
+    public static final ServerVersionRange V1_15 = create("1.15", "1.15.2");
+    public static final ServerVersionRange V1_14 = create("1.14", "1.14.4");
+    public static final ServerVersionRange V1_13 = create("1.13", "1.13.2");
+    public static final ServerVersionRange V1_12 = create("1.12", "1.12.2");
+    public static final ServerVersionRange V1_11 = create("1.11", "1.11.2");
+    public static final ServerVersionRange V1_10 = create("1.10", "1.10.2");
+    public static final ServerVersionRange V1_9 = create("1.9", "1.9.4");
+    public static final ServerVersionRange V1_8 = create("1.8", "1.8.9");
+
+    boolean isInRange(ServerVersion version) {
+        return version.isNewerThanOrSameAs(minInclusive) && version.isOlderThanOrSameAs(maxInclusive);
+    }
+
+    public static ServerVersionRange create(String minInclusive, String maxInclusive) {
+        return new ServerVersionRange(ServerVersion.getOrCreateVersion(minInclusive), ServerVersion.getOrCreateVersion(maxInclusive));
+    }
+
+    @Override
+    public String toString() {
+        return minInclusive + "-" + maxInclusive;
+    }
+}
