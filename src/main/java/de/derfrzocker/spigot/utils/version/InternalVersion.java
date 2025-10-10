d8ad99278939bfd76829f72dd7dcace3e55ffc3a
DerFrZocker <git@derfrzocker.de>
2025-10-10 19:48:19 +0200

    feat(version): Add Version for 1.21.9 and 1.21.10
    Took 1 minute

--- src/main/java/de/derfrzocker/spigot/utils/version/InternalVersion.java
+++ src/main/java/de/derfrzocker/spigot/utils/version/InternalVersion.java
@@ -2,6 +2,7 @@ package de.derfrzocker.spigot.utils.version;
 
 public enum InternalVersion {
 
+    v1_21_R6(ServerVersionRange.create("1.21.9", "1.21.10")),
     v1_21_R5(ServerVersionRange.create("1.21.6", "1.21.8")),
     v1_21_R4(ServerVersionRange.create("1.21.5", "1.21.5")),
     v1_21_R3(ServerVersionRange.create("1.21.4", "1.21.4")),
